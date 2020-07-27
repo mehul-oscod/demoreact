35bf15f58027244542faac385089e44b0d98d2cc
mehul-oscod <mehul@oscod.com>
2020-07-27 18:16:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is changes from cicd
+          This is changes from 
         </p>
         <a
           className="App-link"
