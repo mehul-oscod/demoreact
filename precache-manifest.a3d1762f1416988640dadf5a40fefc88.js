e114f41a80985936b02ff9953721e3d9128a7f86
mehul-oscod <mehul@oscod.com>
2020-07-27 17:03:11 +0530

    Updates

---
+++ precache-manifest.a3d1762f1416988640dadf5a40fefc88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9090389c2babed09eb2d8ed56cd2f6",
+    "url": "/demoreact/index.html"
+  },
+  {
+    "revision": "f53609c9f594153f8e67",
+    "url": "/demoreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/demoreact/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoreact/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53609c9f594153f8e67",
+    "url": "/demoreact/static/js/main.4df9f31e.chunk.js"
+  },
+  {
+    "revision": "b16bf76d187354d489b1",
+    "url": "/demoreact/static/js/runtime-main.68321b56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
